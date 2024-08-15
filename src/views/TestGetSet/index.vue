ec5983e37da0c82bea986f5aa2823c28d5afbc21
ctm <18082020969@163.com>
2024-08-15 16:24:13 +0800

    最后

--- src/views/TestGetSet/index.vue
+++ src/views/TestGetSet/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="TestGetSet">
     <el-input v-model="model.str" />
+    <button @click="onClick">点我</button>
   </div>
 </template>
 
@@ -10,10 +11,21 @@ const model = reactive({
   str: '',
   get _str() {
     return model.str + '+'
+  },
+  set _str(val) {
+    model.str = val.replace('+', '')
   }
 })
 
 watch(() => model._str, (val) => {
-  console.log('变化了 val = ', val)
+  console.log('变化了_str val = ', val)
 })
+
+watch(() => model.str, (val) => {
+  console.log('变化了 str val = ', val)
+})
+
+const onClick = () => {
+  model._str = 'ab+'
+}
 </script>
